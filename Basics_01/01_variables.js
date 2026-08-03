53fc7fe8ae4580ffcdc31a34bf910f881c3e3dab
Ashutosh Ranjan <ashutoshranjan795@gmail.com>
2026-08-03 14:15:16 +0530

    new - codes

---
+++ Basics_01/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 1454547
+let accountEmail = "ashutosh@google.com"
+var  accountPassword = "12435"
+accountCity = "Delhi"
+
+// accountId = 2
+console.log(accountId);
+
+accountEmail = "ashu@ashu.com"
+accountPassword = 8585
+accountCity = "Mumbai"
+
+// console.log(accountEmail);
+// console.log(accountPassword);
+// console.log(accountCity);
+
+// This is very tough to write , in one line we can use  ------
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
