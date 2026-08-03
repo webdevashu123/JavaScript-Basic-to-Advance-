9c1a9658bbb2569895dba9fea8f3da4a85c29ebc
Ashutosh Ranjan <ashutoshranjan795@gmail.com>
2026-08-03 15:44:49 +0530

    Datatypes- Done

--- Basics_01/01_variables.js
+++ Basics_01/01_variables.js
@@ -1,9 +1,12 @@
 const accountId = 1454547
 let accountEmail = "ashutosh@google.com"
 var  accountPassword = "12435"
-accountCity = "Delhi"
+accountCity = "Delhi"// this is possible , but don't use it
 
-// accountId = 2
+let accountState ;
+
+// accountId = 2 - this is not allowed as because const keyword is used to declare this variable
+
 console.log(accountId);
 
 accountEmail = "ashu@ashu.com"
@@ -16,4 +19,9 @@ accountCity = "Mumbai"
 
 // This is very tough to write , in one line we can use  ------
 
-console.table([accountId,accountEmail,accountPassword,accountCity]);
+/*
+Prefer not to use var because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
---
+++ Basics_01/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict";// treat all JS code as newer version (as now it is not necessary)
+
+// alert("9+1") // It can be used in browser - Console
+
+console.log(3+
+  6) // code readability should be high and this is not te way to write the code
+console.log("ashu")
+
+
+let name = "ashu"
+let age = 24
+let isLoggedIn = false
+
+
+//******************** Premitive datatypes ***************************
+//number => 2 to power 53
+//bigint
+
+//string => " "/ ' '
+//boolean => true/false
+//null => standalone value ( It could also be assigned)
+//undefied => when the value is not defined
+//symbol => to find uniquness
+
+
+//
+//object
+
+console.log(typeof null) //- object
+console.log(typeof undefined) //- undefined
